6c2e45706da70de7a5f0d329d4e6c68706688ddc
DansUkVlogs <danielbannister2005@hotmail.com>
2025-04-16 00:48:19 +0100

    removed test not

--- dispatch/dispatch.js
+++ dispatch/dispatch.js
@@ -292,5 +292,3 @@ unsubscribeUnits = listenForUnitsUpdates();
 document.getElementById('unitTypeFilter').addEventListener('change', applyFilters);
 document.getElementById('serviceFilter').addEventListener('change', applyFilters);
 document.getElementById('callsignFilter').addEventListener('input', applyFilters);
-
-// test
